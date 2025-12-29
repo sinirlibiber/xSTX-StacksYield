d2046e7e28cc8c3d95b4f011354d0522aee6ec86
gumusbey <98549582+sinirlibiber@users.noreply.github.com>
2025-12-29 23:59:29 +0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { ResponseLogger } from "@/components/response-logger";
+import { cookies } from "next/headers";
+import { ReadyNotifier } from "@/components/ready-notifier";
+import { Providers } from "./providers";
+import FarcasterWrapper from "@/components/FarcasterWrapper";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const requestId = cookies().get("x-request-id")?.value;
+
+  return (
+        <html lang="en">
+          <head>
+            {requestId && <meta name="x-request-id" content={requestId} />}
+          </head>
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            {/* Do not remove this component, we use it to notify the parent that the mini-app is ready */}
+            <ReadyNotifier />
+            <Providers>
+      <FarcasterWrapper>
+        {children}
+      </FarcasterWrapper>
+      </Providers>
+            <ResponseLogger />
+          </body>
+        </html>
+      );
+}
+
+export const metadata: Metadata = {
+        title: "StacksYield Farcaster App",
+        description: "Create a Farcaster Frame V2 project, StacksYield, a BitFlow-inspired earn platform using Next.js 15, Shadcn UI, and Hiro SDK. Implement a mock pool table and basic frame setup for staking and yield earning.",
+        other: { "fc:frame": JSON.stringify({"version":"next","imageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/thumbnail_a8a7adb3-1aeb-480e-aee4-dc896c2f0f4c-kfmLQkRZi3mV3z8lsn1gOx670cY0yr","button":{"title":"Open with Ohara","action":{"type":"launch_frame","name":"StacksYield Farcaster App","url":"https://empty-seed-445.app.ohara.ai","splashImageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/farcaster/splash_images/splash_image1.svg","splashBackgroundColor":"#ffffff"}}}
+        ) }
+    };
